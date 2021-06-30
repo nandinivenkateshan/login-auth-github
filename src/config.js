e832d534f20a0f097f91dcf636200f8e8e1845f4
Nandini Venkateshan <nandinivsa@gmail.com>
2021-06-30 17:11:24 +0530

    heroku----------fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 export const fetchAuthDetails = {
-  serverUrl: 'http://localhost:8000',
+  serverUrl: 'https://github-login-auth.herokuapp.com',
   authorizationUrl: 'https://github.com/login/oauth/authorize',
   client_id: process.env.REACT_APP_CLIENT_ID,
   redirect_uri: process.env.REACT_APP_REDIRECT_URI
