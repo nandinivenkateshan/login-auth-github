4c78ed51e13dbefaf24036ce511e96c897e1f163
Nandini Venkateshan <nandinivsa@gmail.com>
2021-06-30 17:48:59 +0530

    git auth credentials for heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 export const fetchAuthDetails = {
-  serverUrl: 'https://github-login-auth.herokuapp.com',
+  serverUrl: 'https://login-auth-github.herokuapp.com',
   authorizationUrl: 'https://github.com/login/oauth/authorize',
   client_id: process.env.REACT_APP_CLIENT_ID,
   redirect_uri: process.env.REACT_APP_REDIRECT_URI
