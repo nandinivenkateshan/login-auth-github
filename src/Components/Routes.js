f78ecd115a8407011fb959064008fd17dfee5fbb
nandinivenkateshan <nandinivsa@gmail.com>
2021-06-30 22:15:02 +0530

    Loader implemented

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import App from '../App'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import PageNotFound from './Error/PageNotFound'
-import ServerErr from './Error/ServerErr'
+import Loader from './Loader'
+
+const App = React.lazy(() => import('../App'))
+const PageNotFound = React.lazy(() => import('./Error/PageNotFound'))
+const ServerErr = React.lazy(() => import('./Error/ServerErr'))
 
 function routes () {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={App} />
-        <Route exact path='/about' component={App} />
-        <Route path='/err' component={ServerErr} />
-        <Route component={PageNotFound} />
-      </Switch>
-    </Router>
+    <>
+      <Suspense fallback={<Loader />}>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={App} />
+            <Route exact path='/about' component={App} />
+            <Route path='/err' component={ServerErr} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Router>
+      </Suspense>
+    </>
   )
 }
 export default routes
