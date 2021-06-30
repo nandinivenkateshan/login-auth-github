6b2c9936753cecbb21b20e008785b931ea43acb1
Nandini Venkateshan <nandinivsa@gmail.com>
2021-06-30 16:24:14 +0530

    Deploy heroku

--- server/index.js
+++ server/index.js
@@ -7,8 +7,14 @@ app.use(express.json())
 app.use(cors())
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
+app.use(express.static(path.join(__dirname, '../build')))
+
 const port = process.env.SERVER_PORT || 8080
 
 app.post('/getAccessToken', db.getAccessToken)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+})
 
 app.listen(port, () => console.log(`App running on the port ${port}`))
